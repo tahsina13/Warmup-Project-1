900740a5742810b69f1214a3e5accd32366402ca
KevinCai319 <kevincai263@gmail.com>
2024-02-17 19:53:15 -0500

    ttt cleanup

--- crates/axum-server/src/routers/ttt_router.rs
+++ crates/axum-server/src/routers/ttt_router.rs
@@ -1,10 +1,4 @@
-use axum::{
-    response::Html,
-    routing::{get},
-    extract::{
-        Query,
-    }
-};
+use axum::{extract::Query, response::Html, routing::get};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -14,14 +8,16 @@ struct StartGameForm {
 }
 
 pub fn new_ttt_router() -> axum::Router {
-    axum::Router::new()
-        .route("/", get(get_handler))
+    axum::Router::new().route("/", get(get_handler))
 }
 
 async fn get_handler(Query(query): Query<StartGameForm>) -> Html<String> {
     if query.name.is_some() {
         let board = query.board.unwrap_or_else(|| "".to_string());
-        Html(ui_components::ttt::accept_from_html(query.name.unwrap(), board))
+        Html(ui_components::ttt::accept_from_html(
+            query.name.unwrap(),
+            board,
+        ))
     } else {
         Html(ui_components::ttt::get_form_html())
     }
